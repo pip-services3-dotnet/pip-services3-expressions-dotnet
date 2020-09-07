07e1ae76ed1b92b889aafda4c9481322383a14f2
Sergey Seroukhov <seroukhov@gmail.com>
2020-09-06 23:41:21 -0700

    Simplified function interface

---
+++ src/Calculator/Functions/IFunctionCollection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PipServices3.Expressions.Calculator.Functions
+{
+    public class IFunctionCollection
+    {
+        public IFunctionCollection()
+        {
+        }
+    }
+}
---
+++ src/Calculator/Variables/IVariableCollection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PipServices3.Expressions.Calculator.Variables
+{
+    public class IVariableCollection
+    {
+        public IVariableCollection()
+        {
+        }
+    }
+}
