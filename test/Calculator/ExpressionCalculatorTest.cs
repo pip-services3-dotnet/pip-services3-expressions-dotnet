70d0abef5dc43ee93c3faff94cddec9e97ac41a7
Danyil Tretiakov <daniil.tr@hotmail.com>
2020-09-16 16:15:11 +0300

    Added some ExpressionCalculator tests

--- test/Calculator/ExpressionCalculatorTest.cs
+++ test/Calculator/ExpressionCalculatorTest.cs
@@ -48,6 +48,60 @@ namespace PipServices3.Expressions.Calculator
             result = await calculator.EvaluateAsync();
             Assert.Equal(VariantType.Boolean, result.Type);
             Assert.True(result.AsBoolean);
+
+            calculator.Expression = "NOT(7 IN ARRAY(1,2,3))";
+            result = await calculator.EvaluateAsync();
+            Assert.Equal(VariantType.Boolean, result.Type);
+            Assert.True(result.AsBoolean);
+
+            calculator.Expression = "A = 123 AND b = 'abc'";
+            Assert.Equal("A", calculator.DefaultVariables.FindByName("a").Name);
+            Assert.Equal("b", calculator.DefaultVariables.FindByName("b").Name);
+            calculator.DefaultVariables.FindByName("a").Value = new Variant(123);
+            calculator.DefaultVariables.FindByName("b").Value = new Variant("abc");
+            result = await calculator.EvaluateAsync();
+            Assert.True(result.AsBoolean);
+
+            calculator.Expression = "A = 2 OR b = 'abc'";
+            result = await calculator.EvaluateAsync();
+            Assert.True(result.AsBoolean);
+
+            calculator.Expression = "x IS NOT NULL";
+            calculator.DefaultVariables.FindByName("x").Value = new Variant(123);
+            result = await calculator.EvaluateAsync();
+            Assert.True(result.AsBoolean);
+
+            calculator.Expression = "y IS NULL";
+            result = await calculator.EvaluateAsync();
+            Assert.True(result.AsBoolean);
+
+            calculator.Expression = "x = 123 AND (y != '' OR z IS NULL)";
+            calculator.DefaultVariables.FindByName("x").Value = new Variant(123);
+            calculator.DefaultVariables.FindByName("y").Value = new Variant("");
+            result = await calculator.EvaluateAsync();
+            Assert.True(result.AsBoolean);
+
+            
+            var date = DateTime.UtcNow;
+
+            calculator.Expression = "date1 = date2";
+            calculator.DefaultVariables.FindByName("date1").Value = new Variant(date);
+            calculator.DefaultVariables.FindByName("date2").Value = new Variant(date);
+            result = await calculator.EvaluateAsync();
+            Assert.True(result.AsBoolean);
+
+            calculator.Expression = "date1 > date2";
+            calculator.DefaultVariables.FindByName("date1").Value = new Variant(date.AddDays(1));
+            calculator.DefaultVariables.FindByName("date2").Value = new Variant(date);
+            result = await calculator.EvaluateAsync();
+            Assert.True(result.AsBoolean);
+
+            calculator.Expression = "date1 < date2 AND date2 <= date3";
+            calculator.DefaultVariables.FindByName("date1").Value = new Variant(date.AddDays(-1));
+            calculator.DefaultVariables.FindByName("date2").Value = new Variant(date);
+            calculator.DefaultVariables.FindByName("date3").Value = new Variant(date);
+            result = await calculator.EvaluateAsync();
+            Assert.True(result.AsBoolean);
         }
 
     }
